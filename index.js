8652ecc28cd13b22faa84a02baeca434b6c9ec8b
Oleksii Sytar <oleksii.sytar@gmail.com>
2023-03-26 21:36:47 +0300

    improwements

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const PREFIX = '/'; // Prefix for bot commands
+const PREFIX = "/"; // Prefix for bot commands
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
+client.on("message", (message) => {
   // Ignore messages from other bots or not starting with the prefix
   if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
@@ -14,7 +14,7 @@ client.on('message', message => {
   const args = message.content.slice(PREFIX.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === 'poll') {
+  if (command === "poll") {
     // Get the poll question and options from the arguments
     const question = args[0];
     const options = args.slice(1);
@@ -22,15 +22,17 @@ client.on('message', message => {
     // Create the poll embed message
     const pollEmbed = new Discord.MessageEmbed()
       .setTitle(question)
-      .setDescription(options.map((option, index) => `${index + 1}. ${option}`).join('\n'));
+      .setDescription(
+        options.map((option, index) => `${index + 1}. ${option}`).join("\n")
+      );
 
     // Send the poll message and add reactions for each option
-    message.channel.send(pollEmbed).then(sentMessage => {
+    message.channel.send(pollEmbed).then((sentMessage) => {
       for (let i = 0; i < options.length; i++) {
         sentMessage.react(`${i + 1}\uFE0F\u20E3`);
       }
     });
-  } else if (command === 'survey') {
+  } else if (command === "survey") {
     // Get the survey question and options from the arguments
     const question = args[0];
     const options = args.slice(1);
@@ -38,10 +40,12 @@ client.on('message', message => {
     // Create the survey embed message
     const surveyEmbed = new Discord.MessageEmbed()
       .setTitle(question)
-      .setDescription(options.map((option, index) => `${index + 1}. ${option}`).join('\n'));
+      .setDescription(
+        options.map((option, index) => `${index + 1}. ${option}`).join("\n")
+      );
 
     // Send the survey message and add reactions for each option
-    message.channel.send(surveyEmbed).then(sentMessage => {
+    message.channel.send(surveyEmbed).then((sentMessage) => {
       for (let i = 0; i < options.length; i++) {
         sentMessage.react(`${i + 1}\uFE0F\u20E3`);
       }
